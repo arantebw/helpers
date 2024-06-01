9c7b15f57a287a23bdbe6ea97d68c9fe0f2cda7b
Billy Arante <arantebillywilson@gmail.com>
2024-06-01 13:37:24 +0800

    feat(src): ✨ add the kebabCaseToCapitalized helper function

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 A collection of helper functions.
 
+## transform
+
+Transform a string in kebab-case to capitalized words.
+
+- [kebabCaseToCapitalized.ts](./src/kebabToCapitalized.ts)
+
 ## sorting
 
 Sort a list of `Country` objects using the `country` key.
---
+++ src/kebabCaseToCapitalized.ts
@@ -0,0 +1,20 @@
+const kebabCaseToCapitalized = (str: string) => {
+  if (typeof str !== "string" || str.length === 0) {
+    return ""; // Return an empty string for invalid input
+  }
+
+  // Split the kebab-cased string into an array of words
+  const words = str.split("-");
+
+  // Capitalize each word in the array
+  const capitalizedWords = words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  });
+
+  // Join the array of capitalized words with a space
+  const capitalizedString = capitalizedWords.join(" ");
+
+  return capitalizedString;
+}
+
+export default kebabCaseToCapitalized;
