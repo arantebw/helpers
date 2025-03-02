80f925119a3520bfd1d49512902435e4d7143531
Billy Arante <arantebillywilson@gmail.com>
2025-03-02 23:32:54 +0800

    feat: ✨ add the default exports of several helpers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { default as capitalizedToKebabCase } from "./capitalizedToKebabCase";
 export { default as formatToCurency } from "./formatToCurrency";
+export { default as formatUnixDate } from "./formatUnixDate";
+export { default as generateUniqueKey } from "./generateUniqueKey";
 export { default as kebabCaseToCapitalized } from "./kebabCaseToCapitalized";
+export { default as removeWhiteSpaces } from "./removeWhiteSpaces";
 export { default as sortCountries } from "./sortCountries";
 export { default as stringToArrayOfStrings } from "./stringToArrayOfStrings";
