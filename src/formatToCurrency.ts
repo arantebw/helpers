2faab6d10f8574c9a4e2fdc5586f7258456e05c7
Billy Arante <arantebillywilson@gmail.com>
2025-03-03 18:11:15 +0800

    refactor: ♻️ update formatToCurrency helper to support precision and symbol position

--- src/formatToCurrency.ts
+++ src/formatToCurrency.ts
@@ -1,12 +1,22 @@
-const formatToCurrency = (
+export const formatToCurrency = (
   value: number,
-  locales: string | string[],
+  locales: string | string[] = "lb-LU",
   currency = "EUR",
+  precision = 2,
+  symbolPosition: "before" | "after" = "before",
 ) => {
-  return new Intl.NumberFormat(locales, {
+  const formatted = new Intl.NumberFormat(locales, {
     style: "currency",
     currency,
+    minimumFractionDigits: precision,
+    maximumFractionDigits: precision,
   }).format(value);
+
+  if (symbolPosition === "after") {
+    return formatted.replace(/(\D+)([\d\s.,]+)/, "$2 $1");
+  }
+
+  return formatted;
 };
 
 export default formatToCurrency;
